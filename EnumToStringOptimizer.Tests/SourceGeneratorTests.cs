7eb4e8a20c0025e97faa4ebb5bf62205f59184fd
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-08-21 15:37:50 +0300

    Add tests, fix project structure

---
+++ EnumToStringOptimizer.Tests/SourceGeneratorTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace EnumToStringOptimizer.Tests
+{
+    public class SourceGeneratorTests
+    {
+        [Fact]
+        public void Execute_ShouldNotThrow_WhenRequestsExist()
+        {
+            // Arrange
+            List<string> sources = new()
+            {
+                @"
+using System;
+
+namespace EnumOptimizer.Tests
+{
+    public enum Role
+    {
+        Admin, User
+    }
+}
+",
+                @"
+using System;
+
+namespace EnumOptimizer.Tests.Types
+{
+    public enum MediaType
+    {
+        Image, Video
+    }
+}
+",
+                @"
+using System;
+
+namespace EnumOptimizer.Tests.Types
+{
+    public class TestType
+    {
+        public enum Variant
+        {
+            One, Two
+        }
+    }
+}
+"
+            };
+            Compilation inputCompilation = CreateCompilation(sources);
+            SourceGenerator generator = new();
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+            // Act
+            driver = driver.RunGeneratorsAndUpdateCompilation(inputCompilation, out var outputCompilation, out var diagnostics);
+            GeneratorDriverRunResult _ = driver.GetRunResult();
+            // Assert
+            // do not throws
+        }
+
+        private static Compilation CreateCompilation(List<string> sources)
+        {
+            return CSharpCompilation.Create("compilation",
+                sources.Select(source => CSharpSyntaxTree.ParseText(source)),
+                new[] { MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location) },
+                new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+        }
+    }
+}
---
+++ EnumToStringOptimizerBenchmarks/Program.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace EnumToStringOptimizer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+
+    public enum MediaType
+    {
+        Video, Image, Album, LiveStream
+    }
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private MediaType Type = MediaType.Video;
+        [Benchmark]
+        public string NativeToString() => Type.ToString();
+
+        [Benchmark]
+        public string FastToString() => Type.FastToString();
+    }
+}
